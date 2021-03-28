dfc2e8ba3db7b47d707ee07a9e86ba5d2800aa96
Brandon Risberg <brandon.risberg@gmail.com>
2021-03-28 16:46:51 -0700

    Adds preliminary batch file for backing up Valheim world files

---
+++ AquireLock.bat
@@ -0,0 +1,24 @@
+@ECHO off
+SET "configfile=config.txt"
+IF Not Exist "%configfile%" (Goto :Error)
+
+::Access config properties and read it as Key=Value
+setlocal enabledelayedexpansion
+for /f "tokens=1,* delims== " %%i in (%configfile%) do SET "%%i=%%j"
+
+ECHO %user_email%
+ECHO %gdrive_path%
+ECHO %valheim_worlds_path%
+
+ECHO Copying world data files to Cloud Backup...
+xcopy /y "%valheim_worlds_path%"\TestSync.* "%gdrive_path%"\
+
+PAUSE
+EXIT
+
+::----------- Config Missing Error Handler
+:Error
+cls & Color 4C
+ECHO  Error: Could not find "%configfile%", make sure to create a `config.txt` file in this directory.
+PAUSE
+EXIT
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # FileSyncLock
 Simple Batch utility program for synchronizing data files
+
+## Overview
+
+Simple batch utility for syncing files from the local file system to a cloud back up system. Utilizes a lock file to prevent concurrent modification.
+
