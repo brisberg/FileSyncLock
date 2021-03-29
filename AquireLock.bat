0526a862e9ab4d533a25ee96795448f93de78aaa
Brandon Risberg <brandon.risberg@gmail.com>
2021-03-28 17:20:32 -0700

    Extracts world_name into config file

--- AquireLock.bat
+++ AquireLock.bat
@@ -7,11 +7,12 @@ setlocal enabledelayedexpansion
 for /f "tokens=1,* delims== " %%i in (%configfile%) do SET "%%i=%%j"
 
 ECHO %user_email%
+ECHO %world_name%
 ECHO %gdrive_path%
 ECHO %valheim_worlds_path%
 
 ECHO Copying world data files to Cloud Backup...
-xcopy /y "%valheim_worlds_path%"\TestSync.* "%gdrive_path%"\
+xcopy /y "%valheim_worlds_path%"\"%world_name%".* "%gdrive_path%"\"%world_name%"\
 
 PAUSE
 EXIT
